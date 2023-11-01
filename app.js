1b37d76adf2861aee074f678ffaf5da44dcf2b00
AnnaFusion <anna.bykadorova@fusion-tech.pro>
2023-11-01 09:24:32 +0300

    errors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const userRouter = require('./routes/user.routes');
 const postRouter = require('./routes/post.routes');
 const topicRouter = require('./routes/topic.routes');
 const { sequelize } = require("./models");
+const CustomError = require("./errors");
 const app = express();
 
 app.use(express.json());
@@ -11,12 +12,21 @@ app.use('/api', userRouter);
 app.use('/api', postRouter);
 app.use('/api', topicRouter);
 
+app.use((error,req,res,next) => {
+  res.status(error.status || 500)
+  const message = error.message || 'Something went wrong on the server side';
+res.json({status: error.status, message, stack: error.stack})
+console.log("Error status: ", error.status);
+console.log("Message :", message);
+})
+
+
 app.listen({ port: 3003 }, async () => {
   console.log("Server up on http://localhost:3003");
   try {
   await sequelize.authenticate();
   console.log("Database Connected!");
   } catch (err){    
-    console.error(">>>>>>>>>>>Unable to connect to the database:", err);    
+    console.error(">>>>>>>>>>>Unable to connect to the database:", err); 
   }
 });
--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,70 +1,94 @@
 const { User } = require("../models");
+const  CustomError  = require("../errors");
 
 class UserController {
-    async createUser(req,res) {
-        const { name } = req.body;
-
+    async createUser(req,res,next) {
         try {
-          const user = await User.create({ name });
-      
+          const { name } = req.body;// errorы - косячный параметр 400 и имя- пустая строка 400
+          console.log(name.length)
+          if (!name || !name.length){
+            throw new CustomError('User has wrong name', 400)
+          }
+          const user = await User.create({ name }); // error - юзер не найден 404
+          if(!user){
+            throw new CustomError('User not found', 404)
+          }
           return res.json(user);
         } catch (err) {
-          console.log(">>>>>>", err);
-          return res.status(500).json(err);
+          next(err);
         }
     }
-    async getUsers(req,res) {
+    async getUsers(req,res,next) {
         try {
-            const users = await User.findAll();
-        
+            const users = await User.findAll(); // error - юзеры не найдена
             return res.json(users);
           } catch (err) {
-            console.log(">>>>>>", err);
-            return res.status(500).json({ error: "Something went wrong" });
+            next(err);
           }
     }
-    async getOneUser(req,res) {
-        const id = req.params.id;
-
+    async getOneUser(req,res, next) {
         try {
+          const id = req.params.id;
+
+          if (!isFinite(id)) {
+            throw new CustomError('User id has wrong', 400)
+          }
+
           const user = await User.findOne({
             where: { id },
-            include: "posts",
+            include: "posts",// ошибка юзер не найден
           });
+
+          if(!user) {
+            throw new CustomError('User not found', 404)
+          }
+
+          //throw new CustomError(undefined, 500)
       
           return res.json(user);
         } catch (err) {
-          console.log(">>>>>>", err);
-          return res.status(500).json({ error: "Something went wrong" });
+          next(err);
+          // console.log(">>>>>>", err);
+          // return res.status(500).json({ error: "Something went wrong" });
         }
     }
-    async deleteUser(req,res) {
-        const id = req.params.id;
+    async deleteUser(req,res,next) { // ошибка не предан параметр
+       
         try {
-          const user = await User.findOne({ where: { id } });
-      
+          const id = req.params.id;
+          if (!isFinite(id)) {
+            throw new CustomError('User id has wrong', 400)
+          }
+          const user = await User.findOne({ where: { id } }); // юзер не найден или уже удаоен
+          if(!user) {
+            throw new CustomError('User not found', 404)
+          }
           await user.destroy();
       
           return res.json({ message: "User deleted!" });
         } catch (err) {
-          console.log(">>>>>>", err);
-          return res.status(500).json({ error: "Something went wrong" });
+          next(err);
         }
     }
-    async updateUser(req,res) {
-        const id = req.params.id;
-        const { name } = req.body;
+    async updateUser(req,res,next) {
+        
         try {
+          const id = req.params.id;
+          if (!isFinite(id)) {
+            throw new CustomError('User id is wrong', 400)
+          }
+        const { name } = req.body;
           const user = await User.findOne({ where: { id } });
-      
+          if(!user) {
+            throw new CustomError('User not found', 404)
+          }
           user.name = name;
       
           await user.save();
       
           return res.json(user);
         } catch (err) {
-          console.log(">>>>>>", err);
-          return res.status(500).json({ error: "Something went wrong" });
+          next(err);
         }
     }
 }
