51474ea8b506f9b52b457e8b8522a43e8912fef3
krok-86 <8700478@mail.ru>
2023-11-23 18:19:24 +0300

    auth.me

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -8,7 +8,7 @@ const handleValidationErrors = require('../utils/handleValidationErrors');
 //create user(author)
 router.post('/registration', validation.registerValidation, handleValidationErrors, userController.registrationUser);
 router.post('/authorization',validation.loginValidation, handleValidationErrors, userController.authorizationUser);//handleValidationErrors, loginValidation - it's work?
-router.get('/authorization/me', checkAuth, validation.loginValidation, handleValidationErrors, userController.authorizationMeUser);//where is more add checkAuth? And handleValidationErrors needs here?
+router.get('/authorization/me', checkAuth, userController.authorizationMeUser);//validation.loginValidation, handleValidationErrors, ?
 router.get('/', userController.getUsers);
 router.get('/:id', userController.getOneUser);
 router.delete('/:id', checkAuth, userController.deleteUser);
--- validations/auth.js
+++ validations/auth.js
@@ -19,26 +19,3 @@ registerValidation,
 loginValidation,
 postCreateValidation
 };
-
-// const { body } = require("express-validator");
-
-// const registerValidation = [
-//   body('email', 'invalid email format').isEmail(),
-//   body('password', 'password must have more than 5 symbols').isLength({ min: 5 }), 
-//   body('name', 'name must have more than 5 symbols').isLength({ min: 3 }),
-// ];
-
-// const loginValidation = [
-//   body('email', 'invalid email format').isEmail(),
-//   body('password', 'password must have more than 5 symbols').isLength({ min: 5 })
-// ];
-
-// const postCreateValidation = [
-//   body('postText', 'enter article text' ).isLength({ min: 3 }).isString(),
-// ];
-
-// module.exports = {
-//   registerValidation,
-//   loginValidation,
-//   postCreateValidation
-// };
